63d3c18b1e6f010403777ebaac09cd382b5fb7fd
Riyyi <Riyyi@users.noreply.github.com>
2025-03-11 12:27:10 +0100

    Add project baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import Aura from "@primevue/themes/aura";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
+	app: {
+		baseURL: '/website-vue/',
+	}
+
 	css: [
 		"primeicons/primeicons.css",
 		"~/assets/css/style.css"
